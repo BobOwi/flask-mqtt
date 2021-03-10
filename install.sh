31e6b7086a88d7a68618e415299d55e917b6c46a
BobOwi <bobthebiker@gmail.com>
2021-03-10 18:15:22 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 apt update
-apt install -y python3.9 python3-pip
-apt install -y git
+apt install -y python3.9 python3-pip git
 git clone https://github.com/BobOwi/flask-mqtt.git 
 cd flask-mqtt
 pip3 install -r requirements.txt
